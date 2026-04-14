7d72a588b04955ad311bfbd3c0f7a156724c8658
aigarreta <aigarreta@udesa.edu.ar>
2026-04-14 00:30:06 -0300

    validar y anticipar errores

--- main.py
+++ main.py
@@ -6,20 +6,25 @@ Created on Sun Apr  5 15:36:24 2026
 """
 
 #código principal
-from carga_datos import cargar_datos
-from metricas import calcular_tiempo_total, calcular_promedio_uso, calcular_uso_por_app
-from procesamiento_datos import filtrar_por_participante
-from validacion_datos import validar_registro
+from src.carga_datos import cargar_datos
+from src.metricas import calcular_tiempo_total, calcular_promedio_uso, calcular_uso_por_app
+from src.procesamiento_datos import filtrar_por_participante
+from src.validacion_datos import validar_registro
+from datos import BehaviorTracker_mock_data #no esta bien imoprtado el arch!!
 
-datos = cargar_datos("datos/datos_proyecto.csv")
-datos_validos = []
-for registro in datos:
-	if validar_registro(registro):
-		datos_validos.append(registro)
-id_ingresado = int(input("Ingrese su dni: "))
-datos_por_participante = filtrar_por_participante(datos_validos, id_ingresado)
-promedio_uso= calcular_promedio_uso(datos_por_participante)
-uso_por_app = calcular_uso_por_app(datos_por_participante)
-tiempo_total = calcular_tiempo_total(datos_por_participante)
+def main():
+    
+    datos = cargar_datos(BehaviorTracker_mock_data)
+    datos_validos = []
+    if len(datos) != 0:
+        for registro in datos:
+            if validar_registro(registro):
+                datos_validos.append(registro)
+        
+        id_ingresado = int(input("Ingrese su dni: "))
+        datos_por_participante = filtrar_por_participante(datos_validos, id_ingresado)
+        promedio_uso= calcular_promedio_uso(datos_por_participante)
+        uso_por_app = calcular_uso_por_app(datos_por_participante)
+        tiempo_total = calcular_tiempo_total(datos_por_participante)
 
-print(f" El promedio de uso es de: {promedio_uso}, El tiempo total es de: {tiempo_total}, El uso por app es: {uso_por_app}(todos los tiempos están evaluados en minutos)")
+    print(f" El promedio de uso es de: {promedio_uso}, El tiempo total es de: {tiempo_total}, El uso por app es: {uso_por_app}(todos los tiempos están evaluados en minutos)")
--- src/validacion_datos.py
+++ src/validacion_datos.py
@@ -5,10 +5,10 @@ def validar_registro(registro):
     """
     Qué hace la función:
     Verifica que:
-       - id_participante y  antidad_uso: numeros enteros positivos y no nulos
+       - id_participante y  cantidad_uso: numeros enteros positivos y no nulos
        - tiempo_uso: numero positivo y no nulo
        - app: str
-       - ningún campo esté vacío
+       - los datos (strings) no esten vacíos
     
 
     Parameteros
@@ -18,21 +18,27 @@ def validar_registro(registro):
 
     Retorna
     -------
-   - False: bool
-   si hay algún dato inválido
-   - True: bool
-   si los datos son todos válidos
+   - ValueError si es un dato inválido
+   - True: si los datos son todos válidos
 
     """
     # Verificar que no haya campos vacíos
     for valor in registro.values():
-        if valor == "" or valor is None:
-            return False
+        if valor == "": #para los strings (fecha y app)
+            raise ValueError(" El campo fecha o app es vacío") #me lo dijo chat, aclarar en readme
+            
         # Verificar valores positivos
-        if (registro["id_participante"] <= 0 or
-           registro["cantidad_uso"] <= 0 or
-           registro["tiempo_uso"] <= 0):
-            return False
+            # y verificar dni de 8 dígitos
+        if (registro["id_participante"]) <= 0 or (len(str(registro["id_participante"])) != 8):
+            raise ValueError("El dni debe ser un número positivo de ocho dígitos")
+            
+        if registro["cantidad_uso"] <= 0:
+            raise ValueError("La cantidad de uso debe ser un número positivo")
+            
+        if registro["tiempo_uso"] <= 0:
+            raise ValueError("El tiempo de uso debe ser un número positivo")
+            
+            
     return True
 
 
